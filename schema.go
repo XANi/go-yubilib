95a4c778d84beb1a5bb625bce6ee399bdc98821d
Mariusz Gronczewski <xani666@gmail.com>
2019-11-05 19:31:48 +0100

    make serial + publicname unique, no sane reason there should be a dupe here

--- schema.go
+++ schema.go
@@ -4,19 +4,19 @@ import "time"
 
 // YubikeyKSM is GORM schema with info used by KSM, mostly similar to what Yubico tooling uses.
 type YubikeyKSM struct {
-	SerialNr int `gorm:"UNIQUE,column:serialnr"`
-	PublicName string `gorm:"PRIMARY_KEY;column:publicname"`
-	CreatedAt time.Time `gorm:"column:created"`
-	UpdatedAt time.Time  `gorm:"column:modified"`
-	InternalName string  `gorm:"column:internalname"`
-	AESKey string  `gorm:"column:aeskey;size:32"`
-	LockCode string `gorm:"column:lockcode"`
-	Creator string `gorm:"column:creator"`
-	Active bool  `gorm:"column:active"`
+	SerialNr     int       `gorm:"UNIQUE;column:serialnr;unique_index:serialnr_publicname" json:"serialnr"`
+	PublicName   string    `gorm:"PRIMARY_KEY;column:publicname;unique_index:serialnr_publicname" json:"publicname"`
+	CreatedAt    time.Time `gorm:"column:created"`
+	UpdatedAt    time.Time `gorm:"column:modified"`
+	InternalName string    `gorm:"column:internalname" json:"internalname"`
+	AESKey       string    `gorm:"column:aeskey;size:32" json:"aeskey"`
+	LockCode     string    `gorm:"column:lockcode" json:"lockcode"`
+	Creator      string    `gorm:"column:creator" json:"creator"`
+	Active       bool      `gorm:"column:active"`
 	// whether it is hardware key
 	Hardware bool `gorm:"column:hardware"`
 	// static key if present/used
-	Static string `gorm:"column:static"`
+	Static string `gorm:"column:static" json:"static,omitempty"`
 }
 func (YubikeyKSM) TableName() string {
     return "yubiksm"
@@ -26,21 +26,21 @@ func (YubikeyKSM) TableName() string {
 //YubikeyOTP is
 
 type YubikeyOTP struct {
-  PublicName  string `gorm:"PRIMARY_KEY;column:ykpublicname"`
-  internalName string
-  Active bool `gorm:"column:active"`
-  // names same as in gorm.Model
-  CreatedAt time.Time `gorm:"column:created"`
-  UpdatedAt time.Time `gorm:"column:modified"`
-  // increments after first generation after power on
-  // incerements if use counter overflows, persistent
-  SessionCounter uint16 `gorm:"column:yk_counter"`
-  // increments by 1 each token generation after first (0x00-0xff), overflows into session counter
-  UseCounter uint8`gorm:"column:yk_use"`
-  // 24 bit timestamp
-  YKTSLow uint16 `gorm:"column:yk_low"`
-  YKTSHigh uint8 `gorm:"column:yk_high"`
-  Notes string `gorm:"column:notes"`
+	PublicName   string `gorm:"PRIMARY_KEY;column:ykpublicname"`
+	internalName string
+	Active       bool `gorm:"column:active"`
+	// names same as in gorm.Model
+	CreatedAt time.Time `gorm:"column:created"`
+	UpdatedAt time.Time `gorm:"column:modified"`
+	// increments after first generation after power on
+	// increments if use counter overflows, persistent
+	SessionCounter uint16 `gorm:"column:yk_counter"`
+	// increments by 1 each token generation after first (0x00-0xff), overflows into session counter
+	UseCounter uint8 `gorm:"column:yk_use"`
+	// 24 bit timestamp
+	YKTSLow  uint16 `gorm:"column:yk_low"`
+	YKTSHigh uint8  `gorm:"column:yk_high"`
+	Notes    string `gorm:"column:notes"`
 }
 
 func (YubikeyOTP) TableName() string {
